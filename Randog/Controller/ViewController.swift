af309853f2313c38d5c301ca66edbc4ed25ca17d
oliveira-marcio <emaildomso@gmail.com>
2020-08-20 08:53:56 +0100

    feat: Add DogAPI model

--- Randog/Controller/ViewController.swift
+++ Randog/Controller/ViewController.swift
@@ -14,9 +14,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(DogAPI.Endpoint.randomImageFromAllDogsCollection.url)
     }
-
-
 }
-
---
+++ Randog/Model/DogAPI.swift
@@ -0,0 +1,19 @@
+//
+//  DogAPI.swift
+//  Randog
+//
+//  Created by Márcio Oliveira on 8/20/20.
+//  Copyright © 2020 Márcio Oliveira. All rights reserved.
+//
+
+import Foundation
+
+class DogAPI {
+    enum Endpoint: String {
+        case randomImageFromAllDogsCollection = "https://dog.ceo/api/breeds/image/random"
+        
+        var url: URL {
+            return URL(string: self.rawValue)!
+        }
+    }
+}
