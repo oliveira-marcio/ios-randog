681f92bc62bfef771af82101f9b6328d67422159
oliveira-marcio <emaildomso@gmail.com>
2020-08-20 08:41:16 +0100

    feat: start project

---
+++ Randog/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Randog
+//
+//  Created by Márcio Oliveira on 8/20/20.
+//  Copyright © 2020 Márcio Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
